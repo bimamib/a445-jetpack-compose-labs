0f2fc0991b2d44c8ea71d0c6fc1275d9f27378bc
bimamib <bimaprast1601@gmail.com>
2023-11-02 00:40:34 +0700

    menggunakan currentBackStackEntryAsState

--- navigation-pada-compose/JetReward(starter)/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ navigation-pada-compose/JetReward(starter)/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -11,14 +11,15 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.dicoding.jetreward.ui.navigation.NavigationItem
 import com.dicoding.jetreward.ui.navigation.Screen
@@ -58,12 +59,14 @@ fun JetRewardApp(
 
 @Composable
 private fun BottomBar(
-    navController: NavController,
+    navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
     NavigationBar(
         modifier = modifier,
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
         val navigationItems = listOf(
             NavigationItem(
                 title = stringResource(R.string.menu_home),
@@ -90,7 +93,7 @@ private fun BottomBar(
                     )
                 },
                 label = { Text(item.title) },
-                selected = false,
+                selected = currentRoute == item.screen.route,
                 onClick = {
                     navController.navigate(item.screen.route) {
                         popUpTo(navController.graph.findStartDestination().id) {
