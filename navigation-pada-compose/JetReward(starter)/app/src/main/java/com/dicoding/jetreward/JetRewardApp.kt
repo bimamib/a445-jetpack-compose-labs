d76501d2fe016807a1c18fc26ffb9686762bcf58
bimamib <bimaprast1601@gmail.com>
2023-11-02 00:18:33 +0700

    menambahkan NavHost di dalam Scaffold

--- navigation-pada-compose/JetReward(starter)/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ navigation-pada-compose/JetReward(starter)/app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -1,5 +1,6 @@
 package com.dicoding.jetreward
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
@@ -14,15 +15,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.dicoding.jetreward.ui.navigation.NavigationItem
 import com.dicoding.jetreward.ui.navigation.Screen
+import com.dicoding.jetreward.ui.screen.cart.CartScreen
+import com.dicoding.jetreward.ui.screen.home.HomeScreen
+import com.dicoding.jetreward.ui.screen.profile.ProfileScreen
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
 
 @Composable
 fun JetRewardApp(
     modifier: Modifier = Modifier,
-    navController: NavController = rememberNavController(),
+    navController: NavHostController = rememberNavController(),
 ) {
     Scaffold(
         bottomBar = {
@@ -30,7 +37,21 @@ fun JetRewardApp(
         },
         modifier = modifier
     ) { innerPadding ->
-
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) {
+                HomeScreen()
+            }
+            composable(Screen.Cart.route) {
+                CartScreen()
+            }
+            composable(Screen.Profile.route) {
+                ProfileScreen()
+            }
+        }
     }
 }
 
