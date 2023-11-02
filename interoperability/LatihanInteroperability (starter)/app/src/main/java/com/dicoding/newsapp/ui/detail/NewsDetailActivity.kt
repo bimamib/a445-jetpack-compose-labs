3084259261b92834b8f2d50c7a780f9df2a09d58
bimamib <bimaprast1601@gmail.com>
2023-11-03 06:44:25 +0700

    menghapus code untuk diganti dengan stateless Composable Function

--- interoperability/LatihanInteroperability (starter)/app/src/main/java/com/dicoding/newsapp/ui/detail/NewsDetailActivity.kt
+++ interoperability/LatihanInteroperability (starter)/app/src/main/java/com/dicoding/newsapp/ui/detail/NewsDetailActivity.kt
@@ -6,6 +6,22 @@ import android.view.MenuItem
 import android.webkit.WebViewClient
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import androidx.core.content.IntentCompat
 import com.dicoding.newsapp.R
@@ -16,7 +32,6 @@ import com.dicoding.newsapp.ui.ViewModelFactory
 class NewsDetailActivity : AppCompatActivity() {
 
     private lateinit var newsDetail: NewsEntity
-    private lateinit var binding: ActivityNewsDetailBinding
     private val factory: ViewModelFactory = ViewModelFactory.getInstance(this)
     private val viewModel: NewsDetailViewModel by viewModels {
         factory
@@ -24,43 +39,59 @@ class NewsDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityNewsDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
         newsDetail = IntentCompat.getParcelableExtra(intent, NEWS_DATA, NewsEntity::class.java) as NewsEntity
-
-        binding.topAppBar.title = newsDetail.title
-
-        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.action_bookmark -> {
-                    viewModel.changeBookmark(newsDetail)
-                    true
-                }
-                else -> false
-            }
-        }
-
-        viewModel.bookmarkStatus.observe(this) { status ->
-            setBookmarkState(status)
-        }
-
-        binding.webView.webViewClient = WebViewClient()
-        binding.webView.loadUrl(newsDetail.url.toString())
-
-        viewModel.setNewsData(newsDetail)
-    }
-
-    private fun setBookmarkState(state: Boolean) {
-        val menuItem = binding.topAppBar.menu.findItem(R.id.action_bookmark)
-        if (state) {
-            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_bookmarked_white)
-        } else {
-            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_bookmark_white)
-        }
     }
 
     companion object {
         const val NEWS_DATA = "data"
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NewsDetailContent(
+    title: String,
+    url: String,
+    bookmarkStatus: Boolean,
+    updateBookmarkStatus: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = title,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
+                },
+                actions = {
+                    IconButton(onClick = updateBookmarkStatus) {
+                        Icon(
+                            painter = if (bookmarkStatus) {
+                                painterResource(R.drawable.ic_bookmarked_white)
+                            } else {
+                                painterResource(R.drawable.ic_bookmark_white)
+                            },
+                            contentDescription = stringResource(R.string.save_bookmark),
+                        )
+                    }
+                }
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding).fillMaxSize()) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NewsDetailContentPreview() {
+    MaterialTheme {
+        NewsDetailContent("New News", "www.dicoding.com", false, {})
     }
 }
